14dac21df57b16bfd8c7ef858122730bdf967fbd
null-version-nova <ethcoyle@gmail.com>
2024-01-28 00:33:01 -0600

    hey the preprocessor almost works. forge is a bit silly though so its not quite ready

--- common/src/main/kotlin/nullversionnova/novaslibrary/registry/EntityRegistry.kt
+++ common/src/main/kotlin/nullversionnova/novaslibrary/registry/EntityRegistry.kt
@@ -1,15 +1,15 @@
 package nullversionnova.novaslibrary.registry
 
 import dev.architectury.registry.level.entity.EntityAttributeRegistry
-import net.minecraft.core.registries.Registries
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.MobCategory
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier.Builder
 import net.minecraft.world.level.Level
+import nullversionnova.novaslibrary.abstraction.RegistryAccessor
 import nullversionnova.novaslibrary.util.RegistryDelegate
 
-open class EntityRegistry(id: String) : GenericRegistry<EntityType<*>>(id, Registries.ENTITY_TYPE) {
+open class EntityRegistry(id: String) : GenericRegistry<EntityType<*>>(id, RegistryAccessor.ENTITY_TYPE) {
     fun <T : LivingEntity> register(path : String, builder : EntityType.Builder<T>, attributes: () -> Builder) : RegistryDelegate<EntityType<T>> {
         val key = register(path) { builder.build(path) }
         whenRegistered {
